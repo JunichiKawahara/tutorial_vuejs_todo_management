8c915d92b5ae33c6e8a99ef2805320951089f539
JunichiKawahara <kawahara-j@hi-ad.jp>
2018-11-11 18:31:54 +0900

    ひととおり終了

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,16 +3,22 @@
   <div>
     {{ msg }}
     <form>
-      <button>ADD TASK</button>
-      <button>DELETE FINISHED TASKS</button>
-      <p>input: <input type="text"></p>
-      <p>task:</p>
+      <button v-on:click="addTodo">ADD TASK</button>
+      <button v-on:click="removeTodo">DELETE FINISHED TASKS</button>
+      <p>input: <input type="text" v-model="newTodo"></p>
+      <p>task: {{ newTodo }}</p>
     </form>
     <div class="task-list">
-      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-router</label>
-      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vuex</label>
-      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-loader</label>
-      <label class="task-list__item--checked"><input type="checkbox" checked><button>EDIT</button>awesome-vue</label>
+      <label 
+        v-for="todo in todos"
+        v-bind:key="todo.id"
+        v-bind:class="{ 'task-list__item--checked': todo.done }"
+        class="task-list__item">
+        <input type="checkbox" v-model="todo.done">
+        <input type="checkbox" v-model="todo.editing">
+        <input v-if="todo.editing" v-model="todo.text" v-on:keyup.enter="todo.editing = !todo.editing">
+        <span v-else>{{ todo.text }}</span>
+      </label>
     </div>
   </div>
 </template>
@@ -22,7 +28,36 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      todos: [
+        { id: 1, text: 'vue-router', done: false, editing: false },
+        { id: 2, text: 'vuex', done: false, editing: false },
+        { id: 3, text: 'vue-loader', done: false, editing: false },
+        { id: 4, text: 'awesome-vue', done: true, editing: false },
+      ],
+      newTodo: ''
+    }
+  },
+  methods: {
+    addTodo(event) {
+      let text = this.newTodo && this.newTodo.trim()
+      if (!text) {
+        return;
+      }
+      this.todos.push({
+        id: this.todos.length + 1,
+        text: text,
+        done: false,
+        editing: false
+      });
+      this.newTodo = '';
+    },
+    removeTodo(event) {
+      for (let i = this.todos.length - 1; i >= 0; i--) {
+        if (this.todos[i].done) {
+          this.todos.splice(i, 1);
+        }
+      }
     }
   }
 }
@@ -50,5 +85,9 @@ export default {
       color: #85a6c6;
     }
   }
+}
+.task-list__item--checked {
+  color: #003366;
+  font-weight: bold;
 }
 </style>
